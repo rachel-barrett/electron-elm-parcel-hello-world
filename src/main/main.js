b8c16fb2f6fba28179db579faf56dd7e7ebe1dff
Rachel Barrett <rachel_barrett@live.co.uk>
2020-11-12 16:03:33 +0000

    Add default app closing behaviour

--- src/main/main.js
+++ src/main/main.js
@@ -24,6 +24,19 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
 ipcMain.on('channel', (event, args) => {
   console.log(args) //logs the message to terminal
   event.returnValue = 'Hello from the main process!'
